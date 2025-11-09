af1986491adcfa72e253490a942f28b9f4614485
Lucas Root <lucasroot@Lucass-MacBook-Air-6.local>
2025-11-08 18:23:36 -0600

    created turt.h and turt.c files

---
+++ turt.c
@@ -0,0 +1,14 @@
+#include "turt.h"
+#include <stdio.h>
+
+int main(int ac, char **av) {
+    //REPL
+    //Read-Eval-Print Loop for Turtle graphics commands
+    while (0) {
+        //1) get line
+
+        //2) get tokens
+    }
+
+    return EXIT_SUCCESS;
+}
